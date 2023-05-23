2fd0bcda7e9c1466dfaa80974bc0bd4cb3cf4c03
DVNO <dvno@live.se>
2023-05-23 13:29:41 +0200

    Firs steps in project Admin Panel

---
+++ admin_panel/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.scss'
+
+const App = () => {
+  return (
+    <div>Admin Panel</div>
+  )
+}
+
+export default App
---
+++ admin_panel/src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import App from './App.jsx'
+import './Main.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)

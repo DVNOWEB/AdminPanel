264f04b6db1d79ed535399da5c4c79bcd951ae8a
DVNO <dvno@live.se>
2023-06-02 19:58:22 +0200

    small fix

--- admin_panel/src/pages/App.test.jsx
+++ admin_panel/src/pages/App.test.jsx
@@ -1,14 +1,14 @@
-// import { expect, it } from 'vitest'
+import { expect, it } from 'vitest'
 
-// // create a function to test
-// const func = () => {
-//   return true
-// }
+// create a function to test
+const func = () => {
+  return true
+}
 
 
-// // test the function with vitest
-// it('should render without crashing', () => {
-//   const result = func()
+// test the function with vitest
+it('should render without crashing', () => {
+  const result = func()
 
-//   expect(result).toBe(true)
-// })
+  expect(result).toBe(true)
+})
--- admin_panel/vite.config.js
+++ admin_panel/vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFilesAfterEnv: ['./tests/setup.js'],
+    setupFilesAfterEnv: './tests/setup.js',
   },
 })
